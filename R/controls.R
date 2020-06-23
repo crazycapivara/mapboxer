03f87379d2d6cda604fa8c40012c5d71d8b5d08f
skuethe <crazycapivara@gmail.com>
2020-06-23 08:15:44 +0200

    Fullscreen Control

--- R/controls.R
+++ R/controls.R
@@ -1,25 +1,32 @@
+#' Add a control to the map
+#' @inheritParams set_view_state
+#' @param control_name The (class) name of the control.
+#' @param ... The properties of the control.
+#' @param pos The position of the control, one of \code{top-left}, \code{top-right},
+#'   \code{bottom-right} or \code{bottom-left}.
+#' @export
 add_control <- function(map, control_name, ..., pos = NULL) {
   map %>%
     invoke_method("addControl", controlName = control_name, pos = pos, options = list(...))
 }
 
-#' Add a navigation control to the map
-#'
-#' @inheritParams set_view_state
-#' @param ... The properties of the control.
-#' @param pos The position of the control, one of \code{top-left}, \code{top-right},
-#'   \code{bottom-right} or \code{bottom-left}.
 #' @export
+#' @name add_control
 add_navigation_control <- function(map, ..., pos = NULL) {
   map %>%
     add_control("NavigationControl", ..., pos = pos)
 }
 
-#' Add a scale control to the map
-#'
-#' @inheritParams add_navigation_control
 #' @export
+#' @name add_control
 add_scale_control <- function(map, ..., pos = NULL) {
   map %>%
     add_control("ScaleControl", ..., pos = pos)
+}
+
+#' @export
+#' @name add_control
+add_fullscreen_control <- function(map, pos = NULL) {
+  map %>%
+    add_control("FullscreenControl", pos = pos)
 }
