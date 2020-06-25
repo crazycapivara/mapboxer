2e6c7ce85bbaef7ccf7b30001ae48a08614c690b
skuethe <crazycapivara@gmail.com>
2020-06-25 07:40:57 +0200

    Add rdocs for 'add_fill_layer'

--- R/layers_fill.R
+++ R/layers_fill.R
@@ -1,5 +1,29 @@
+#' Add a fill layer to the map
+#' @param source A Mapbox source. Uses the source from the \link{mapboxer} object if no source is supplied.
+#' @param fill_antialias (paint) Whether or not the fill should be antialiased.
+#' @param fill_color (paint) The color of the filled part of this layer.
+#'   This color can be specified as rgba with an alpha component and
+#'   the color's opacity will not affect the opacity of the 1px stroke, if it is used.
+#' @param fill_opacity (paint) The opacity of the entire fill layer.
+#'   In contrast to the \code{fill_color}, this value will also affect the 1px stroke around the fill,
+#'   if the stroke is used.
+#' @param fill_outline_color (paint) The outline color of the fill.
+#'   Matches the value of \code{fill_color} if unspecified.
+#' @param fill_pattern (paint) ...
+#' @param fill_sort_key (layout) Sorts features in ascending order based on this value.
+#'   Features with a higher sort key will appear above features with a lower sort key.
+#' @param fill_translate (paint) The geometry's offset.
+#'   Values are [x, y] where negatives indicate left and up, respectively.
+#' @param fill_translate_anchor (paint) Controls the frame of reference for \code{fill_translate},
+#'   either \code{map} or \code{viewport}.
+#' @param visibility (layout) Whether the layer should be displayed.
+#' @inheritParams add_popup
+#' @param id The unique id of the layer.
+#' @example inst/api-reference/fill-layer.R
+#' @export
 add_fill_layer <- function(
                            map,
+                           source = NULL,
                            fill_antialias = TRUE,
                            fill_color = "#000000",
                            fill_opacity = 1,
@@ -27,6 +51,7 @@ add_fill_layer <- function(
   style <- list(
     id = id,
     type = "fill",
+    source = source,
     paint = purrr::compact(paint),
     layout = purrr::compact(layout)
   )
--- inst/api-reference/fill-layer.R
+++ inst/api-reference/fill-layer.R
@@ -1,20 +1,16 @@
-fill_style <- list(
-  id = "melbourne",
-  type = "fill",
-  source = "melbourne",
-  paint = list(
-    "fill-color" = c("get", "fillColor"),
-    "fill-opacity" = 0.7
+map <- geojsonsf::geo_melbourne %>%
+  as_mapbox_source() %>%
+  mapboxer() %>%
+  set_view_state(
+    lng =  144.9624,
+    lat = -37.8105,
+    zoom = 10,
+    pitch = 35
+  ) %>%
+  add_fill_layer(
+    fill_color = c("get", "fillColor"),
+    fill_opacity = 0.6,
+    popup = "Area: {{AREASQKM}} km<sup>2</sup>"
   )
-)
 
-data <- geojsonsf::geo_melbourne
-data <- geojsonsf::geojson_sf(geojsonsf::geo_melbourne)
-class(data)
-source <- as_mapbox_source(data)
-class(source$data)
-
-mapboxer() %>%
-  set_view_state(lng =  144.9624, lat = -37.8105) %>%
-  add_source(source, "melbourne") %>%
-  add_layer(fill_style, popup = "Area: {{AREASQKM}} km<sup>2</sup>") #%>% add_popup("melbourne", "Area: {{AREASQKM}} km<sup>2</sup>")
+if (interactive()) map
