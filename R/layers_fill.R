3858d5c3de37a2af53e78ba3f15d4b7287467bd8
skuethe <crazycapivara@gmail.com>
2020-06-25 19:35:59 +0200

    Update rdocs fill layer

--- R/layers_fill.R
+++ R/layers_fill.R
@@ -10,16 +10,17 @@
 #'   if the stroke is used.
 #' @param fill_outline_color (paint) The outline color of the fill.
 #'   Matches the value of \code{fill_color} if unspecified.
-#' @param fill_pattern (paint) ...
+#' @param fill_pattern (paint) Name of image in sprite to use for drawing image fills.
 #' @param fill_sort_key (layout) Sorts features in ascending order based on this value.
 #'   Features with a higher sort key will appear above features with a lower sort key.
 #' @param fill_translate (paint) The geometry's offset.
 #'   Values are [x, y] where negatives indicate left and up, respectively.
-#' @param fill_translate_anchor (paint) Controls the frame of reference for \code{fill_translate},
-#'   either \code{map} or \code{viewport}.
+#' @param fill_translate_anchor (paint) Controls the frame of reference for \code{fill_translate}.
+#'   One of "map", "viewport".
 #' @param visibility (layout) Whether the layer should be displayed.
 #' @inheritParams add_popup
 #' @param id The unique id of the layer.
+#' @seealso \url{https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#fill}
 #' @example inst/api-reference/fill-layer.R
 #' @export
 add_fill_layer <- function(
@@ -27,8 +28,8 @@ add_fill_layer <- function(
                            source = NULL,
                            filter = NULL,
                            fill_antialias = TRUE,
-                           fill_color = "#000000",
-                           fill_opacity = 1,
+                           fill_color = NULL,
+                           fill_opacity = NULL,
                            fill_outline_color = NULL,
                            fill_pattern = NULL,
                            fill_sort_key = NULL,
