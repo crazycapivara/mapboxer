7ac4b073f2cb598b097d2be2f1bca7664173cfaa
skuethe <crazycapivara@gmail.com>
2020-06-30 08:13:29 +0200

    Update Shiny example

--- examples/shiny.R
+++ examples/shiny.R
@@ -13,12 +13,15 @@ server <- function(input, output) {
     quakes %>%
       as_mapbox_source(lng = "long", lat = "lat") %>%
       mapboxer(center = c(176.9, -24.655), zoom = 4, style = use_background_style()) %>%
-      add_circle_layer(circle_color = "red", popup = "{{mag}}", id = "quakes")
+      add_circle_layer(circle_color = "red", popup = "{{mag}}", id = "quakes") %>%
+      add_mouse_position_control("Lng: {{lng}}, Lat: {{lat}}")
   })
 
   observeEvent(input$slider, {
+    row <- quakes[sample(1:nrow(quakes), 1), ]
     mapboxer_proxy("map") %>%
       set_filter("quakes", list(">", "mag", input$slider)) %>%
+      add_marker(row$long, row$lat) %>%
       send_update(hi = "folks")
   })
 }
