8ab5a1ccdbf3dec4fd0ff610a7a98c57774797f8
skuethe <crazycapivara@gmail.com>
2020-10-01 18:24:25 +0200

    Update earthquake example

--- examples/cirlce-layer_clusters.R
+++ examples/cirlce-layer_clusters.R
@@ -1,25 +1,30 @@
-# data_url <- "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
-# earthquakes <- sf::st_read(data_url)
+data_url <- "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+earthquakes <- sf::st_read(data_url)
 
-quakes %>%
+earthquakes %>%
   as_mapbox_source(
-    lat = "lat",
-    lng = "long",
     cluster = TRUE,
     clusterMaxZoom = 14,
     clusterRadius = 50
   ) %>%
-  mapboxer(center = c(176.9, -24.655), zoom = 4) %>%
+  mapboxer(
+    center = c(-103.59179687498357, 40.66995747013945),
+    zoom = 3
+  ) %>%
+  add_navigation_control() %>%
   add_circle_layer(
-    circle_color = "red",
+    circle_color = list(
+      'step', c("get", "point_count"),
+      "#51bbd6", 100,
+      "#f1f075", 750,
+      "#f28cb1"
+    ),
     circle_radius = list(
-      "step",
-      list("get", "point_count"),
-      10, 15,
-      20, 30,
-      30, 80,
+      "step", c("get", "point_count"),
+      20, 100,
+      30, 750,
       40
     ),
-    filter = list("has", "point_count"),
-    popup = "{{point_count}}"
-  )
+    filter = c("has", "point_count"),
+    popup = "{{point_count_abbreviated}}"
+  ) #%>% add_layer()
