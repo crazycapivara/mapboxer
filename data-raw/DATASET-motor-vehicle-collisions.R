d3237a4c2e8ab7c18b56094f426fb0d3ca825246
skuethe <crazycapivara@gmail.com>
2020-10-01 09:48:07 +0200

    Extract code

--- data-raw/DATASET-motor-vehicle-collisions.R
+++ data-raw/DATASET-motor-vehicle-collisions.R
@@ -1,11 +1,3 @@
-## code to prepare `DATASET` dataset goes here
-
-data_url <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
-vancouver_blocks <- sf::st_read(data_url)
-saveRDS(vancouver_blocks, "data-raw/vancouver-blocks.rds")
-
-#usethis::use_data("DATASET")
-
 # https://opendata.cityofnewyork.us/
 
 # Motor Vehicle Collisions - Crashes
