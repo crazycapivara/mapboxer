a4b007ace3c05bfe26abe09fd29e20877cc62ef1
skuethe <crazycapivara@gmail.com>
2020-10-13 14:11:48 +0200

    Add basemap providers

---
+++ R/basemap-styles.R
@@ -0,0 +1,28 @@
+PROVIDERS <- list(
+  Carto = list(
+    url = "https://basemaps.cartocdn.com/gl/%s-gl-style/style.json",
+    themes = c("dark-matter", "voyager", "positron")
+  ),
+  Mapbox = list(
+    url = "mapbox://styles/mapbox/%s",
+    themes = c("streets-v11", "outdoors-v11", "light-v10", "dark-v10", "satellite-v9", "satellite-steets-v11")
+  )
+)
+
+build_style_url_list <- function(provider) {
+  urls <- list()
+  for (theme in provider$themes) {
+    urls[[gsub("-", "_", theme)]] <- sprintf(provider$url, theme)
+  }
+
+  urls
+}
+
+#' Get basemap providers
+#' @export
+get_basemap_providers <- function() {
+ list(
+   Carto = build_style_url_list(PROVIDERS$Carto),
+   Mapbox = build_style_url_list(PROVIDERS$Mapbox)
+  )
+}
