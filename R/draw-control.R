782ca11879458e5b732682f39b1629bbb9e822f6
skuethe <crazycapivara@gmail.com>
2020-10-24 09:49:11 +0200

    Support sf objects in draw control

--- R/draw-control.R
+++ R/draw-control.R
@@ -1,10 +1,14 @@
 #' Add a draw control to the map
 #' @inheritParams add_control
-#' @param data (experimental) A \code{GeoJSON} object.
+#' @param data (experimental) A \code{GeoJSON} or \link[sf] object.
 #' @seealso \url{https://github.com/mapbox/mapbox-gl-draw/blob/main/docs/API.md}
 #'   for available options for the draw control.
 #' @export
 add_draw_control <- function(map, ..., pos = NULL, data = NULL) {
+  if (inherits(data, "sf")) {
+    data <- geojsonsf::sf_geojson(data)
+  }
+
   map$dependencies <- c(map$dependencies, use_deps("mapbox-gl-draw"))
   invoke_method(map, "addDrawControl", pos = pos, options = list(...), data = data)
 }
