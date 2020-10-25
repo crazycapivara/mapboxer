dad53289bdd5aa17065bd1e906d7292acfdab4e3
skuethe <crazycapivara@gmail.com>
2020-10-25 09:43:49 +0100

    Add helper to create 'us_states' data set

---
+++ data-raw/DATASET-us-states.R
@@ -0,0 +1,12 @@
+url_us_states <- "https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson"
+
+us_states <- sf::st_read(url_us_states)
+
+mapboxer(center = c(-100.486052, 37.830348), zoom = 2) %>%
+  add_fill_layer(
+    id = "us",
+    source = as_mapbox_source(us_states),
+    fill_color = "blue",
+    fill_opacity = 0.4
+  ) %>%
+  add_tooltips("us", "{{STATE_NAME}}")
