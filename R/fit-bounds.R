80e5ff0254de6242bf09ed84eba4e311435ffaf6
skuethe <crazycapivara@gmail.com>
2020-10-28 11:58:01 +0100

    Support bbox objects in 'fit_bounds'

--- R/fit-bounds.R
+++ R/fit-bounds.R
@@ -1,8 +1,13 @@
 #' Fit the map to a bounding box
 #' @param map A \link{mapboxer} object.
-#' @param bounds The bounding box as a vector in [west, south, east, north] order.
+#' @param bounds The bounding box as a vector in [west, south, east, north] order
+#'   or a \code{bbox} object.
 #' @param ... Optional arguments, see \url{https://docs.mapbox.com/mapbox-gl-js/api/map/#map#fitbounds}.
 #' @export
 fit_bounds <- function(map, bounds, ...) {
+  if (inherits(bounds, "bbox")) {
+    bounds %<>% unname()
+  }
+
   invoke_method(map, "fitBounds", bounds = bounds, options = list(...))
 }
