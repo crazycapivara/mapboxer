ede82df3576c17aa15c6f3dbd5f01995141455bd
skuethe <crazycapivara@gmail.com>
2020-11-01 09:36:35 +0100

    Add first approach for 'as_mapboxer_viz'

---
+++ R/as-mapboxer-viz.R
@@ -0,0 +1,9 @@
+as_mapboxer_viz <- function(data, style = basemaps$Carto$voyager, ...) {
+  as_mapbox_source(data) %>%
+    mapboxer(
+      style = style,
+      bounds = sf::st_bbox(data)
+    ) %>%
+    add_navigation_control() %>%
+    add_circle_layer(...)
+}
