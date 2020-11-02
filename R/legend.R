92fb55d6256322be7533c36cf55498e7583f264f
skuethe <crazycapivara@gmail.com>
2020-11-02 08:11:36 +0100

    Export legend control

--- R/legend.R
+++ R/legend.R
@@ -1,3 +1,10 @@
+#' Add a legend control to the map
+#' @param map A \link{mapboxer} object.
+#' @param colors The colors of the legend items.
+#' @param labels The labels of the legend items.
+#' @param title The title of the legend.
+#' @inheritParams add_control
+#' @export
 add_legend_control <- function(map, colors, labels, title = NULL,
                                pos = c("bottom-right", "bottom-left", "top-left", "top-right")) {
   items <- lapply(1:length(colors), function(i) list(color = colors[i], label = labels[i]))
