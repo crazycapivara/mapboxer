5416bd5bc0a66c252bec7a9068974c33063b2a2b
skuethe <crazycapivara@gmail.com>
2020-11-02 19:38:28 +0100

    Add rdocs for deck.gl layer

--- R/experimental_deck-gl-layer.R
+++ R/experimental_deck-gl-layer.R
@@ -1,3 +1,11 @@
+#' Add a deck.gl layer to the map (experimental)
+#' @param map A \link{mapboxer} object
+#' @param type The type of the deck.gl layer, e. g. \code{ScatterplotLayer}.
+#' @param data The data object.
+#' @param ... The properties of the layer.
+#' @param id The unique ID of the layer
+#' @example examples/api-reference/deckgl-layer.R
+#' @export
 add_deckgl_layer <- function(map, type, data, ..., id = "deckgl-layer") {
   map %<>%
     add_deps(use_deps("deck-gl"))
