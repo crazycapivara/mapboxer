cc5af5a25146b53ec7fb41d6ac427e579390f892
skuethe <crazycapivara@gmail.com>
2020-11-03 19:52:32 +0100

    Filter layer on slider change event

--- R/control-panel.R
+++ R/control-panel.R
@@ -1,8 +1,12 @@
 add_control_panel <- function(map, ...) {
   invoke_method(map, "addCustomControl", controlName = "XYZControl",
-                options = list(elem_range_slider(0, 6, 1, 2)))
+                options = list(...))
 }
 
-elem_range_slider <- function(min, max, step, value) {
+elem_range_slider <- function(min, max, step, value = min) {
   list(min = min, max = max, step = step, value = value)
+}
+
+on_change_filter <- function(elem, layer_id, property, operator = "==") {
+  modifyList(elem, list(mapboxLayerId = layer_id, mapboxProperty = property, mapboxOperator = operator))
 }
--- javascript/src/custom-controls/xyz-control.js
+++ javascript/src/custom-controls/xyz-control.js
@@ -18,7 +18,7 @@ export default class XYZControl {
     Object.assign(slider, this._options[0]);
     slider.onchange = (e) => console.log(slider.value, e.target.value);
     */
-    const slider = createSlider(this._options[0]);
+    const slider = this._createSlider(this._options[0]);
     this._container.append(slider);
 
     return this._container;
@@ -28,12 +28,18 @@ export default class XYZControl {
     this._container.parentNode.removeChild(this._container);
     this._map = undefined;
   }
+
+  _createSlider(props) {
+    const slider = document.createElement("input");
+    slider.type = "range";
+    Object.assign(slider, props);
+    slider.onchange = (e) => {
+      // e.target
+      const expression = [ slider.mapboxOperator, slider.mapboxProperty, parseInt(slider.value) ];
+      console.log(expression);
+      this._map.setFilter(slider.mapboxLayerId, expression);
+    };
+    return slider;
+  }
 }
 
-function createSlider(props) {
-  const slider = document.createElement("input");
-  slider.type = "range";
-  Object.assign(slider, props);
-  slider.onchange = (e) => console.log(e.target.value);
-  return slider;
-}
