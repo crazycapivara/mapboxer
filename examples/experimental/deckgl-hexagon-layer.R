df4f9ef4e0af211f5d7d6f556de62fbedf1df7f3
skuethe <crazycapivara@gmail.com>
2020-11-03 08:08:59 +0100

    Add deck.gl hexagon layer example

---
+++ examples/experimental/deckgl-hexagon-layer.R
@@ -0,0 +1,17 @@
+# deck.gl HexagonLayer
+
+mapboxer(
+  center = c(-73.9165, 40.7114),
+  zoom = 10,
+  pitch = 35
+) %>%
+  add_deckgl_layer(
+    type = "HexagonLayer",
+    data = motor_vehicle_collisions_nyc,
+    extruded = TRUE,
+    radius = 300,
+    elevationScale = 4,
+    getPosition = "@=[{{lng}}, {{lat}}]",
+    opacity = 0.5
+  ) %>%
+  add_navigation_control()
