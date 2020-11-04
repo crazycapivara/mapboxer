b8111191f33f4ee0e22e6686f911821e08cc393d
skuethe <crazycapivara@gmail.com>
2020-11-04 14:55:30 +0100

    Add second slider to control panel example

--- examples/experimental/sliders.R
+++ examples/experimental/sliders.R
@@ -1,12 +1,15 @@
 LAYER_ID <- "crashes"
 
+mvc <- motor_vehicle_collisions_nyc
+mvc$day <- lubridate::day(mvc$date)
+
 slider <- elem_range_slider(min = 0, max = 6, step = 1, value = 3) %>%
   on_change_filter(LAYER_ID, property = "injured", operator = ">=")
 
-slider2 <- elem_range_slider(min = 0, max = 6, step = 1, value = 4) %>%
-  on_change_filter(LAYER_ID, property = "injured", operator = "<=")
+slider2 <- elem_range_slider(min = min(mvc$day), max = max(mvc$day), step = 1) %>%
+  on_change_filter(LAYER_ID, property = "day", operator = "==")
 
-as_mapboxer_viz(motor_vehicle_collisions_nyc, coords = c("lng", "lat"), layer_id = LAYER_ID) %>%
+as_mapboxer_viz(mvc, coords = c("lng", "lat"), layer_id = LAYER_ID) %>%
   add_control_panel(
     slider,
     slider2,
