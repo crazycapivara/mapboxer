41ff9a17f3dbacc0727a9b1bcbccb8811d3f57aa
gregleleu <33321678+gregleleu@users.noreply.github.com>
2022-09-05 10:51:57 -0400

    Downgrade mapboxgl to last 1.x.x

--- inst/htmlwidgets/lib/mapbox-gl/get-mapbox-gl.sh
+++ inst/htmlwidgets/lib/mapbox-gl/get-mapbox-gl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget https://unpkg.com/mapbox-gl@2.10.0/dist/mapbox-gl.js
-wget https://unpkg.com/mapbox-gl@2.10.0/dist/mapbox-gl.css
+wget https://unpkg.com/mapbox-gl@1.13.2/dist/mapbox-gl.js -O mapbox-gl.js
+wget https://unpkg.com/mapbox-gl@1.13.2/dist/mapbox-gl.css -O mapbox-gl.css
 
